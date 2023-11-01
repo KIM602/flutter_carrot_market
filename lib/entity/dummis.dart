47d727d4a944d6eb46e6b435574ddd59ee3723c0
KIM602 <kyk940608@gmail.com>
2023-11-01 11:13:47 +0900

    알림 / 물건 상세 화면

--- lib/entity/dummis.dart
+++ lib/entity/dummis.dart
@@ -1,5 +1,5 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/entity/post/vo_product_post.dart';
+import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
 import 'package:fast_app_base/entity/product/product_status.dart';
 import 'package:fast_app_base/entity/product/vo_product.dart';
 import 'package:fast_app_base/entity/user/vo_address.dart';
@@ -30,29 +30,43 @@ final user3 = User(
   profileUrl: picSum(700),
 );
 
-final product1 = Product(
-    user1,
-    "아이폰13",
-    700000,
-    '깨끗하게 잘 쓰떤 물건이에요.'
-        '잘 쓰면 좋겠습니다'
-        '감사합니다',
-    ProductStatus.normal,
+final product1 = Product(user1, "아이폰13", 700000, ProductStatus.normal,
     [picSum(200), picSum(201), picSum(202)]);
 
-final product2 = Product(user1, "갤럭시", 1700000, '페이가 아주 편하고, 자동 통화 녹음이 됩니다.',
-    ProductStatus.normal, [picSum(500), picSum(501), picSum(502), picSum(503)]);
+final product2 = Product(user1, "갤럭시", 1700000, ProductStatus.normal,
+    [picSum(500), picSum(501), picSum(502), picSum(503)]);
 
-final product3 = Product(user1, '스마트 티비', 2000, '화질이 아주 좋아요',
-    ProductStatus.normal, [picSum(800), picSum(801), picSum(802), picSum(803)]);
+final product3 = Product(user1, '스마트 티비', 2000, ProductStatus.normal,
+    [picSum(800), picSum(801), picSum(802), picSum(803)]);
 
-final post1 = ProductPost(product1.user, product1, '글의 내용입니다.', const Address('서울시 특별한 주소', '앱동'), 3, 2,
+final post1 = SimpleProductPost(
+    1,
+    product1.user,
+    product1,
+    '글의 내용입니다.',
+    const Address('서울시 특별한 주소', '앱동'),
+    3,
+    2,
     DateTime.now().subtract(30.minutes));
 
-final post2 = ProductPost(product2.user, product2, '글의 내용입니다.', const Address('서울시 특별한 주소', '플러터동'),
-    30, 120, DateTime.now().subtract(30.minutes));
+final post2 = SimpleProductPost(
+    2,
+    product2.user,
+    product2,
+    '글의 내용입니다.',
+    const Address('서울시 특별한 주소', '플러터동'),
+    30,
+    120,
+    DateTime.now().subtract(30.minutes));
 
-final post3 = ProductPost(product3.user, product3, '글의 내용입니다.', const Address('서울시 특별한 주소', '다트시'),
-    10, 350, DateTime.now().subtract(5.minutes));
+final post3 = SimpleProductPost(
+    3,
+    product3.user,
+    product3,
+    '글의 내용입니다.',
+    const Address('서울시 특별한 주소', '다트시'),
+    10,
+    350,
+    DateTime.now().subtract(5.minutes));
 
 final postList = [post1, post2, post3, post1, post2, post3];
--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -3,6 +3,7 @@ import 'package:fast_app_base/entity/dummis.dart';
 import 'package:fast_app_base/screen/main/fab/floating_daagn_button.riverpod.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_product_post_item.dart';
+import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -51,6 +52,15 @@ class _HomeFragmentState extends ConsumerState<HomeFragment> {
                 .toList(),
             child: Text(title),
           ),
+
+          actions: [
+            IconButton(
+              onPressed: () {
+                Nav.push(const NotificationScreen());
+              },
+              icon: const Icon(Icons.notifications_none_rounded),
+            )
+          ],
         ),
         Expanded(
           child: ListView.separated(
